28ce9490b2e2382b6a73d3d5815799a637c2467f
Gnatogryz <plague@go2.pl>
2017-07-05 17:07:06 +0200

    Pool, zmiana nazewnictwa

--- Assets/Scripts/Dispatcher.cs
+++ Assets/Scripts/Dispatcher.cs
@@ -3,71 +3,174 @@ using UnityEngine;
 using System.Linq;
 
 namespace AAAA {
-	public sealed class Thread : MonoBehaviour {
 
-		static Thread instance;
-
-		bool killAllThreads = false;
-		public static bool killAll { get { return instance.killAllThreads; } }
-
-		volatile List<System.Threading.Thread> threads = new List<System.Threading.Thread>();
-		volatile Queue<System.Action> dispatch = new Queue<System.Action>();
-
-		[RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
-		static void Initialize() {
-			instance = new GameObject("Threading").AddComponent<Thread>();
-			instance.gameObject.hideFlags = HideFlags.HideInHierarchy;
-			DontDestroyOnLoad(instance.gameObject);
+	public static class Threader {
+		public static System.Threading.Thread Run(System.Threading.ThreadStart job) {
+			return Internal.Dispatcher.Run(job);
 		}
 
-		void FixedUpdate() {
-			if (dispatch.Count > 0) {
-				lock (dispatch) {
-					while (dispatch.Count > 0) {
-						dispatch.Dequeue()();
+		public static void Dispatch(System.Action job) {
+			Internal.Dispatcher.Dispatch(job);
+		}
+	}
+
+	public class Worker {
+		Queue<System.Action> jobQueue;
+		object mutex;
+		System.Threading.Thread[] loopThreads;
+		int threadCount;
+
+		public bool killed { get; private set; }
+
+		bool[] busy;
+
+		public int busyThreadCount {
+			get {
+				return busy.Where(b => b).Count();
+			}
+		}
+
+		public Worker(int numberOfThreads) {
+			jobQueue = new Queue<System.Action>();
+			mutex = new object();
+			Internal.Dispatcher.onKillThreads += Kill;
+
+			threadCount = numberOfThreads;
+			loopThreads = new System.Threading.Thread[threadCount];
+
+			busy = new bool[threadCount];
+
+			for (int i = 0; i < threadCount; i++) {
+				int idx = i;
+				busy[i] = false;
+				loopThreads[i] = new System.Threading.Thread(() => InternalLoop(idx));
+				loopThreads[i].Start();
+			}
+		}
+
+		public void Kill() {
+			Internal.Dispatcher.onKillThreads -= Kill;
+			killed = true;		
+		}
+
+		public void Run(System.Action job) {
+			lock (mutex) {
+				jobQueue.Enqueue(job);
+			}
+		}
+
+		public void Dispatch(System.Action job) {
+			Internal.Dispatcher.Dispatch(job);
+		}
+
+
+		void InternalLoop(int idx) {
+			int i = idx;
+			System.Threading.Thread.Sleep(i);
+
+			int jobCount;
+
+			while (!killed) {
+				lock (mutex) {
+					jobCount = jobQueue.Count;
+				}
+
+				if (jobCount > 0) {
+					busy[i] = true;
+					System.Action job;
+					lock (mutex) {
+						job = jobQueue.Dequeue();
+					}
+					try {
+						job.Invoke();
+					} catch (System.Exception e) {
+						Debug.LogError(e.Message + "\n" + e.StackTrace);
+					}
+				} else {
+					busy[i] = false;
+				}
+
+				System.Threading.Thread.Sleep(0);
+			}
+			Debug.Log("[THREADING] Stopping worker thread " + (i + 1));
+		}
+	}
+
+
+	namespace Internal {
+		public sealed class Dispatcher : MonoBehaviour {
+
+			static Dispatcher instance;
+
+			bool killAllThreads = false;
+			public static bool killAll { get { return instance.killAllThreads; } }
+
+			volatile List<System.Threading.Thread> threads = new List<System.Threading.Thread>();
+			volatile Queue<System.Action> dispatch = new Queue<System.Action>();
+
+			static object mutex;
+
+			[RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+			static void Initialize() {
+				mutex = new object();
+				instance = new GameObject("Threading").AddComponent<Dispatcher>();
+				instance.gameObject.hideFlags = HideFlags.HideInHierarchy;
+				DontDestroyOnLoad(instance.gameObject);
+			}
+
+			public static event System.Action onKillThreads;
+
+			void FixedUpdate() {
+				if (dispatch.Count > 0) {
+					lock (mutex) {
+						while (dispatch.Count > 0) {
+							dispatch.Dequeue()();
+						}
 					}
 				}
+
+				threads.RemoveAll(th => th.IsAlive == false);
 			}
 
-			threads.RemoveAll(th => th.IsAlive == false);
-		}
+			void OnDisable() {
+				KillAllThreads();
+			}
 
-		void OnDisable() {
-			KillAllThreads();
-		}
-
-		void OnDestroy() {
-			KillAllThreads();
-		}
+			void OnDestroy() {
+				KillAllThreads();
+			}
 
 
-		void KillAllThreads() {
-			killAllThreads = true;
-			int i = 0;
-			foreach (var th in threads) {
-				if (th.IsAlive) {
-					th.Interrupt();
-					th.Join(100);
-					i++;
+			void KillAllThreads() {
+				if (onKillThreads != null)
+					onKillThreads();
+				killAllThreads = true;
+				int i = 0;
+				foreach (var th in threads) {
+					if (th.IsAlive) {
+						th.Interrupt();
+						th.Join(100);
+						i++;
+					}
+				}
+				if (i > 0) {
+					Debug.Log("[THREADING] Aborting " + i + " threads.\n");
 				}
 			}
-			if (i > 0) {
-				Debug.Log("[THREADING] Aborting " + i + " threads.\n");
+
+
+			public static System.Threading.Thread Run(System.Threading.ThreadStart what) {
+				var th = new System.Threading.Thread(what);
+				instance.threads.Add(th);
+				th.Start();
+				return th;
 			}
-		}
 
 
-		public static System.Threading.Thread Run(System.Threading.ThreadStart what) {
-			var th = new System.Threading.Thread(what);
-			instance.threads.Add(th);
-			th.Start();
-			return th;
-		}
-
-
-		public static void Dispatch(System.Action what) {
-			lock (instance.dispatch) {
-				instance.dispatch.Enqueue(what);
+			public static void Dispatch(System.Action what) {
+				lock (mutex) {
+					instance.dispatch.Enqueue(what);
+				}
 			}
 		}
 	}
--- Assets/Scripts/Tester.cs
+++ Assets/Scripts/Tester.cs
@@ -5,23 +5,48 @@ using AAAA;
 
 public class Tester : MonoBehaviour {
 
+	Worker worker;
+	int finishedThreads = 0;
+	public int busyThreads;
+
 	// Use this for initialization
 	void Start () {
-		int i = 100;
-		while (--i > -1) {
+
+
+	worker = new Worker(8);
+
+	worker.Run(() => {
+		Debug.Log("Kurwa");
+		worker.Dispatch(() => Debug.Log("Mać"));
+	});
+
+		int i = -1;
+		while (++i < 500) {
 			int idx = i;
-			Thread.Run(() => {
-				int ź = -9999;
-				while (++ź < 9999) { }					
-				Debug.Log("Started " + idx);
-				Thread.Dispatch(() => {
-					Debug.Log("Finished " + idx);
+			worker.Run(() => {
+				int ź = -10;
+				while (++ź < 20) {
+					var a = Mathf.Sqrt(Mathf.Sin(1119) % 4 % 3.5f) % 9;
+					var b = Mathf.Acos(Mathf.Sin(a));
+					Debug.LogWarning(b);
+				}
+
+				//Debug.Log("Started " + idx);
+
+				if (i==100) {
+					Debug.Log(GetComponent<Rigidbody>().freezeRotation);
+				}
+
+				worker.Dispatch(() => {
+					//Debug.Log("Finished " + idx);
+					finishedThreads++;
 				});
 			});
 		}
 	}
 
-	// Update is called once per frame
-	void Update () {
+
+	void Update() {
+		busyThreads = worker.busyThreadCount;
 	}
 }
